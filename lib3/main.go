8d1bd8558a6e92fec6b52ddfa706528f24074fbb
Jairo Mendes <jairo.mendes@sumup.com>
2024-08-12 14:36:26 +0200

    Test

--- lib3/main.go
+++ lib3/main.go
@@ -1,14 +1,9 @@
-package main
+package lib3
 
 import (
 	"github.com/jairomfj/multimod-lib/lib1"
 	"github.com/jairomfj/multimod-lib/lib2"
 )
-
-func main() {
-	lib1.Say()
-	lib2.Say()
-}
 
 func Say() {
 	lib1.Say()
